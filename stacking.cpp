85da32b4f87abe65cff8656affd6fef68dfdedf0
Marchjul <marchellogiovani28@gmail.com>
2026-05-14 18:01:39 +0700

    if pop

--- stacking.cpp
+++ stacking.cpp
@@ -36,5 +36,15 @@ class stack
         }
 
         void pop()
+        {
+            if (isEmpty())
+        {
+            cout << "Stack is empty." << endl;
+        }
+
+        Node *temp = top;
+        top = top->next;
+        cout << "Popped value: " << top->data << endl;
+        }
 
 };
